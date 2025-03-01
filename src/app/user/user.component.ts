1b33ad11d167960260ee1f0d0c13e93b2840b281
Avishka <avishka.madushanka123@gmail.com>
2025-03-02 05:26:55 +0530

    user components

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummyusers';
+
+const randomIndexNumber = () =>{
+  return Math.floor(Math.random() * DUMMY_USERS.length);
+}
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  selectedUser = DUMMY_USERS[randomIndexNumber()];
+
+}
