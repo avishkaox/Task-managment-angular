ab6e2fdfd716d57489703a60c616ccbb9908679a
Avishka <avishka.madushanka123@gmail.com>
2025-03-13 00:47:09 +0530

    btnclick

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,28 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummyusers';
 
-const randomIndexNumber = () =>{
+const randomIndexNumber = () => {
   return Math.floor(Math.random() * DUMMY_USERS.length);
-}
+};
 
 @Component({
   selector: 'app-user',
   imports: [],
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-
   selectedUser = DUMMY_USERS[randomIndexNumber()];
 
-  get imageUrl(){
+  get imageUrl() {
     return 'users/' + this.selectedUser.avatar;
   }
 
-  onSelectedUser(){
-    console.log('User selected');
+  onSelectedUser() {
+    const randomIndexNumber = () => {
+      return Math.floor(Math.random() * DUMMY_USERS.length);
+    };
+    this.selectedUser = DUMMY_USERS[randomIndexNumber()];
   }
-
 }
